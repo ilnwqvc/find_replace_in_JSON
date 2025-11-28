879ab76d616334904c477e43cb03bac9b9adbc76
ilnwqvc <rageentic.cs@gmail.com>
2025-11-28 21:35:16 +0400

    Сделал генератор чисел, разработка генератора джейсон файлов

--- index.cpp
+++ index.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
+#include <fstream>
 #include <vector>
-#include <>
+#include <string>
+#include <random>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+//генератор количества файлов
+int CountFiles(int min_range, int max_range){
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> cnt_files(min_range, max_range);
+    return cnt_files(gen);
+}
+
+int main() {
+    int m, mx;
+    cin>>m >> mx;
+    cout<<CountFiles(m, mx);
+    return 0;
+}
