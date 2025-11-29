3d7684a24c975444473e824b4697e45af69da353
ilnwqvc <rageentic.cs@gmail.com>
2025-11-29 23:20:28 +0400

    начал писать функцию поиска в файле

--- find_replace/main.cpp
+++ find_replace/main.cpp
@@ -9,8 +9,28 @@
 
 using namespace std;
 
+string find_in_file(string founder){
+    while (true){
+        int i = 1;
+        string line;
+        string filename = "GenJson/data"+to_string(i)+".json";
+        ifstream fin(filename);
+        while(getline(fin, line)){
+        size_t pos_start_substr = (line.find("\"content\":\""))+10;
+        size_t pos_end_substr = (line.find("\"}"));
+        size_t len_subsrt = (pos_end_substr-pos_start_substr-1);
+        size_t pos_start_name = (line.find("\"name\":\""))
+        string sub = line.substr(pos_start_substr, len_subsrt);
+        if (sub.find(founder)){
+            cout << filename << "   " << 
+        }
+        }
+
+    }
+}
+
 int main(){
     GenerateJSON(400000, 500000); // генерация джейсон файлов перед запуском, после работы они удаляются и при новом запуске генерируются новые
-    
+
 
 }
