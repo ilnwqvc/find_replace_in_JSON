959b1fd005be52352d8757654c8916fdb226c9e9
ilnwqvc <rageentic.cs@gmail.com>
2025-11-30 16:32:36 +0400

    разнес папки

--- find_replace/main.cpp
+++ find_replace/main.cpp
@@ -1,4 +1,4 @@
-#include "GenJson/gen_json.h"
+#include "gen_json.h"
 #include <iostream>
 #include <vector>
 #include <iomanip>
@@ -9,28 +9,32 @@
 
 using namespace std;
 
-string find_in_file(string founder){
+void find_in_file(const string& founder){
     while (true){
         int i = 1;
         string line;
         string filename = "GenJson/data"+to_string(i)+".json";
         ifstream fin(filename);
         while(getline(fin, line)){
-        size_t pos_start_substr = (line.find("\"content\":\""))+10;
-        size_t pos_end_substr = (line.find("\"}"));
-        size_t len_subsrt = (pos_end_substr-pos_start_substr-1);
-        size_t pos_start_name = (line.find("\"name\":\""))
-        string sub = line.substr(pos_start_substr, len_subsrt);
-        if (sub.find(founder)){
-            cout << filename << "   " << 
+            size_t pos_start_substr = (line.find("\"content\":\""))+10;
+            size_t pos_end_substr = (line.find("\"}"));
+            size_t len_subsrt = (pos_end_substr-pos_start_substr-1);
+            size_t pos_start_name = (line.find("\"name\":\""))+8;
+            size_t pos_end_name = (line.find("\",\""));
+            size_t len_name = (pos_start_name-pos_end_name);
+            string name = line.substr(pos_start_name, len_name);
+            string sub = line.substr(pos_start_substr, len_subsrt);
+            if (sub.find(founder)){
+                cout << filename << "   " << name << sub << endl;
+            }
         }
-        }
-
     }
 }
 
 int main(){
     GenerateJSON(400000, 500000); // генерация джейсон файлов перед запуском, после работы они удаляются и при новом запуске генерируются новые
-
-
+    string need_found;
+    cout << "Введите искомую подстроку";
+    find_in_file(need_found);
+    return 0;
 }
